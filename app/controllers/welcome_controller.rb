bd4209890ddabcd6bd837c0bb53bfd97037a1e68
Pierce <pierce.dejong45@gmail.com>
2018-06-18 15:38:34 -0600

    Added Fifa api

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,22 @@
 class WelcomeController < ApplicationController
+
   def index
+    require 'rest_client'
+    response = RestClient.get 'http://worldcup.sfg.io/teams/results', {:accept => :json}
+    response = JSON.parse(response)
+
+    # Loop through all of the teams
+    response.each do |r|
+      # Loop through all of the users
+      User.all.each do |u|
+        # Check if the user has the team
+        if u.teams.find_by(name: r["country"])
+          # If so calc the teams new value
+          points = r["wins"] - r["losses"]
+          u.teams.find_by(name: r["country"]).update(value: points)
+        end
+      end
+    end
   end
 
 
