a1768ea1fd3099a68727b1cb39d3603b493f2fd2
Pierce <pierce.dejong45@gmail.com>
2018-06-18 12:14:57 -0600

    Added table to htmlk page of all players and teams.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
 
   has_many :teams
-  
+
+
+  def total
+    total = 0
+    self.teams.all.each do |t|
+      total += t.value
+    end
+
+    return total
+  end
+
 end
