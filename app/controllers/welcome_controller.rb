616205224f1180854144a40d4646f39ed140e308
Pierce <pierce.dejong45@gmail.com>
2018-06-18 17:01:43 -0600

    Error handle when API can not be found

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,20 +2,26 @@ class WelcomeController < ApplicationController
 
   def index
     require 'rest_client'
-    response = RestClient.get 'http://worldcup.sfg.io/teams/results', {:accept => :json}
-    response = JSON.parse(response)
+    begin
+      response = RestClient.get 'http://worldcup.sfg.io/teams/results', {:accept => :json}
+      response = JSON.parse(response)
 
-    # Loop through all of the teams
-    response.each do |r|
-      # Loop through all of the users
-      User.all.each do |u|
-        # Check if the user has the team
-        if u.teams.find_by(name: r["country"])
-          # If so calc the teams new value
-          points = r["wins"] - r["losses"]
-          u.teams.find_by(name: r["country"]).update(value: points)
+      # Loop through all of the teams
+      response.each do |r|
+        # Loop through all of the users
+        User.all.each do |u|
+          # Check if the user has the team
+          if u.teams.find_by(name: r["country"])
+            # If so calc the teams new value
+            points = r["wins"] - r["losses"]
+            u.teams.find_by(name: r["country"]).update(value: points)
+          end
         end
       end
+      @load = true
+    rescue
+      logger.error("Failed To Read API")
+      @load = false
     end
   end
 
