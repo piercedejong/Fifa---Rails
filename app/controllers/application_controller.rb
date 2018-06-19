a179719002d20d665190aa4ad0af455dc8fe7207
Pierce <pierce.dejong45@gmail.com>
2018-06-19 10:05:49 -0600

    Added curent leader, css updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_leader
+
+  def current_leader
+    top = 0
+
+    User.all.each do |u|
+      if u.total > top
+        top = u.total
+      end
+    end
+    leader = ""
+    User.all.each do |u|
+      if u.total.eql? top
+        leader+=u.name+", "
+      end
+    end
+    leader = leader[0...-2]
+    return leader
+  end
 end
