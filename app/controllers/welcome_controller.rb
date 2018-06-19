a1b382793534b1525392279d04bf717b626aa757
Pierce <pierce.dejong45@gmail.com>
2018-06-19 08:54:38 -0600

    Moved seeding to correct file

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,7 @@ class WelcomeController < ApplicationController
   def index
     require 'rest_client'
     begin
+      # Get the API data
       response = RestClient.get 'http://worldcup.sfg.io/teams/results', {:accept => :json}
       response = JSON.parse(response)
 
@@ -19,6 +20,7 @@ class WelcomeController < ApplicationController
         end
       end
       @load = true
+    # Error handle
     rescue
       logger.error("Failed To Read API")
       @load = false
@@ -27,142 +29,6 @@ class WelcomeController < ApplicationController
 
 
   def create_user
-    Team.all.each do |a|
-      a.destroy
-    end
-
-    User.all.each do |u|
-      u.destroy
-    end
-
-    user = [
-      {uid: 0, name:"EdCruise"},
-      {uid: 1, name:"El Presidente"},
-      {uid: 2, name:"Dentista"},
-      {uid: 3, name:"Shark Sieloff"},
-      {uid: 4, name:"EdTeach"},
-      {uid: 5, name:"The Undergrad"},
-      {uid: 6, name:"Zimon Ibrah"},
-    ]
-
-    user.each do |u|
-      User.create(u)
-    end
-
-    edward = [
-      {value: 0, name:"Argentina"},
-      {value: 0, name:"Belgium"},
-      {value: 0, name:"Columbia"},
-      {value: 0, name:"Egypt"},
-      {value: 0, name:"England"},
-      {value: 0, name:"France"},
-      {value: 0, name:"Germany"},
-      {value: 0, name:"Mexico"},
-      {value: 0, name:"Portugal"},
-      {value: 0, name:"Serbia"},
-      {value: 0, name:"Uruguay"},
-      {value: 0, name:"Senegal"},
-    ]
-
-    frank = [
-      {value: 0, name:"Argentina"},
-      {value: 0, name:"Belgium"},
-      {value: 0, name:"Brazil"},
-      {value: 0, name:"Croatia"},
-      {value: 0, name:"Denmark"},
-      {value: 0, name:"England"},
-      {value: 0, name:"France"},
-      {value: 0, name:"Germany"},
-      {value: 0, name:"Poland"},
-      {value: 0, name:"Spain"},
-      {value: 0, name:"Uruguay"},
-      {value: 0, name:"Korea Republic"},
-    ]
-
-    andrew = [
-      {value: 0, name:"Argentina"},
-      {value: 0, name:"Belgium"},
-      {value: 0, name:"Brazil"},
-      {value: 0, name:"Columbia"},
-      {value: 0, name:"Denmark"},
-      {value: 0, name:"France"},
-      {value: 0, name:"Germany"},
-      {value: 0, name:"Nigeria"},
-      {value: 0, name:"Portugal"},
-      {value: 0, name:"Spain"},
-      {value: 0, name:"Uruguay"},
-      {value: 0, name:"Costa Rica"},
-    ]
-
-    rob = [
-      {value: 0, name:"Argentina"},
-      {value: 0, name:"Belgium"},
-      {value: 0, name:"Brazil"},
-      {value: 0, name:"Columbia"},
-      {value: 0, name:"England"},
-      {value: 0, name:"France"},
-      {value: 0, name:"Germany"},
-      {value: 0, name:"Poland"},
-      {value: 0, name:"Russia"},
-      {value: 0, name:"Spain"},
-      {value: 0, name:"Uruguay"},
-      {value: 0, name:"Switzerland"},
-    ]
-
-    ed = [
-      {value: 0, name:"Argentina"},
-      {value: 0, name:"Brazil"},
-      {value: 0, name:"Columbia"},
-      {value: 0, name:"Croatia"},
-      {value: 0, name:"England"},
-      {value: 0, name:"France"},
-      {value: 0, name:"Germany"},
-      {value: 0, name:"Poland"},
-      {value: 0, name:"Portugal"},
-      {value: 0, name:"Spain"},
-      {value: 0, name:"Uruguay"},
-      {value: 0, name:"Switzerland"},
-    ]
-
-    pierce = [
-      {value: 0, name:"Argentina"},
-      {value: 0, name:"Belgium"},
-      {value: 0, name:"Brazil"},
-      {value: 0, name:"England"},
-      {value: 0, name:"France"},
-      {value: 0, name:"Germany"},
-      {value: 0, name:"Peru"},
-      {value: 0, name:"Poland"},
-      {value: 0, name:"Portugal"},
-      {value: 0, name:"Spain"},
-      {value: 0, name:"Uruguay"},
-      {value: 0, name:"Switzerland"},
-    ]
-
-    simon = [
-      {value: 0, name:"Argentina"},
-      {value: 0, name:"Belgium"},
-      {value: 0, name:"Brazil"},
-      {value: 0, name:"Croatia"},
-      {value: 0, name:"Egypt"},
-      {value: 0, name:"England"},
-      {value: 0, name:"France"},
-      {value: 0, name:"Germany"},
-      {value: 0, name:"Portugal"},
-      {value: 0, name:"Spain"},
-      {value: 0, name:"Uruguay"},
-      {value: 0, name:"No Bonus Team"},
-    ]
-
-    teams = [edward,frank,andrew,rob,ed,pierce,simon]
-
-    User.all.each do |x|
-      team = teams[x.uid]
-      team.each do |t|
-        x.teams.create(t)
-      end
-    end
-
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,138 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Team.all.each do |a|
+  a.destroy
+end
+
+User.all.each do |u|
+  u.destroy
+end
+
+user = [
+  {uid: 0, name:"EdCruise"},
+  {uid: 1, name:"El Presidente"},
+  {uid: 2, name:"Dentista"},
+  {uid: 3, name:"Shark Sieloff"},
+  {uid: 4, name:"EdTeach"},
+  {uid: 5, name:"The Undergrad"},
+  {uid: 6, name:"Zimon Ibrah"},
+]
+
+user.each do |u|
+  User.create(u)
+end
+
+edward = [
+  {value: 0, name:"Argentina"},
+  {value: 0, name:"Belgium"},
+  {value: 0, name:"Colombia"},
+  {value: 0, name:"Egypt"},
+  {value: 0, name:"England"},
+  {value: 0, name:"France"},
+  {value: 0, name:"Germany"},
+  {value: 0, name:"Mexico"},
+  {value: 0, name:"Portugal"},
+  {value: 0, name:"Serbia"},
+  {value: 0, name:"Uruguay"},
+  {value: 0, name:"Senegal"},
+]
+
+frank = [
+  {value: 0, name:"Argentina"},
+  {value: 0, name:"Belgium"},
+  {value: 0, name:"Brazil"},
+  {value: 0, name:"Croatia"},
+  {value: 0, name:"Denmark"},
+  {value: 0, name:"England"},
+  {value: 0, name:"France"},
+  {value: 0, name:"Germany"},
+  {value: 0, name:"Poland"},
+  {value: 0, name:"Spain"},
+  {value: 0, name:"Uruguay"},
+  {value: 0, name:"Korea Republic"},
+]
+
+andrew = [
+  {value: 0, name:"Argentina"},
+  {value: 0, name:"Belgium"},
+  {value: 0, name:"Brazil"},
+  {value: 0, name:"Colombia"},
+  {value: 0, name:"Denmark"},
+  {value: 0, name:"France"},
+  {value: 0, name:"Germany"},
+  {value: 0, name:"Nigeria"},
+  {value: 0, name:"Portugal"},
+  {value: 0, name:"Spain"},
+  {value: 0, name:"Uruguay"},
+  {value: 0, name:"Costa Rica"},
+]
+
+rob = [
+  {value: 0, name:"Argentina"},
+  {value: 0, name:"Belgium"},
+  {value: 0, name:"Brazil"},
+  {value: 0, name:"Colombia"},
+  {value: 0, name:"England"},
+  {value: 0, name:"France"},
+  {value: 0, name:"Germany"},
+  {value: 0, name:"Poland"},
+  {value: 0, name:"Russia"},
+  {value: 0, name:"Spain"},
+  {value: 0, name:"Uruguay"},
+  {value: 0, name:"Switzerland"},
+]
+
+ed = [
+  {value: 0, name:"Argentina"},
+  {value: 0, name:"Brazil"},
+  {value: 0, name:"Colombia"},
+  {value: 0, name:"Croatia"},
+  {value: 0, name:"England"},
+  {value: 0, name:"France"},
+  {value: 0, name:"Germany"},
+  {value: 0, name:"Poland"},
+  {value: 0, name:"Portugal"},
+  {value: 0, name:"Spain"},
+  {value: 0, name:"Uruguay"},
+  {value: 0, name:"Switzerland"},
+]
+
+pierce = [
+  {value: 0, name:"Argentina"},
+  {value: 0, name:"Belgium"},
+  {value: 0, name:"Brazil"},
+  {value: 0, name:"England"},
+  {value: 0, name:"France"},
+  {value: 0, name:"Germany"},
+  {value: 0, name:"Peru"},
+  {value: 0, name:"Poland"},
+  {value: 0, name:"Portugal"},
+  {value: 0, name:"Spain"},
+  {value: 0, name:"Uruguay"},
+  {value: 0, name:"Switzerland"},
+]
+
+simon = [
+  {value: 0, name:"Argentina"},
+  {value: 0, name:"Belgium"},
+  {value: 0, name:"Brazil"},
+  {value: 0, name:"Croatia"},
+  {value: 0, name:"Egypt"},
+  {value: 0, name:"England"},
+  {value: 0, name:"France"},
+  {value: 0, name:"Germany"},
+  {value: 0, name:"Portugal"},
+  {value: 0, name:"Spain"},
+  {value: 0, name:"Uruguay"},
+  {value: 0, name:"No Bonus Team"},
+]
+
+teams = [edward,frank,andrew,rob,ed,pierce,simon]
+
+User.all.each do |x|
+  team = teams[x.uid]
+  team.each do |t|
+    x.teams.create(t)
+  end
+end
