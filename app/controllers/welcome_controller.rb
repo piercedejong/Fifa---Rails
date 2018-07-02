d5cd40207612ad63f09363fe1eb681b227659714
Pierce <pierce.dejong45@gmail.com>
2018-07-02 12:22:45 -0600

    Added wins for elimination

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,39 +11,23 @@ class WelcomeController < ApplicationController
     require 'rest_client'
     start = Time.now
     begin
-      # Get the API data
-      # response = RestClient.get 'http://worldcup.sfg.io/teams/results', {:accept => :json}
-      # response = JSON.parse(response)
-      # User.all.each do |u|
-      #   u.teams.all.each do |t|
-      #     @team = response.find{|key| key["country"] == t.name }
-      #     if @team != nil
-      #       points = @team["wins"] - @team["losses"]
-      #       if t.value != points
-      #         t.update(value:points)
-      #         @change = true
-      #       end
-      #     end
-      #   end
-      # end
       @group_table = RestClient.get 'https://worldcup.sfg.io/teams/group_results', {:accept => :json}
       @group_table = JSON.parse(@group_table)
 
-      @team_table = RestClient.get 'https://worldcup.sfg.io/teams/results', {:accept => :json}
-      @team_table = JSON.parse(@team_table)
+      @match_table = RestClient.get 'https://worldcup.sfg.io/matches', {:accept => :json}
+      @match_table = JSON.parse(@match_table)
+      @knockout_table = @match_table.select {|key| key["stage_name"] != "First stage"}
       User.all.each do |u|
         u.teams.all.each do |t|
           @group = @group_table.find{|key| key["letter"] == t.group}["ordered_teams"]
           @team = @group.find{|key| key["country"] == t.name}
-          @team2 = @team_table.find{|key| key["country"] == t.name}
+          @knockout = @knockout_table.select {|key| key["winner"]== t.name}
           if @team != nil
-            knockoutWins = (@team2["wins"] - @team["wins"]) * 2
-            points = knockoutWins + @team["wins"] - @team["losses"]
+            points = @team["wins"] + @knockout.length - @team["losses"]
             if t.value != points
               t.update(value:points)
               @change = true
             end
-
             if @team != @group.first and @team != @group.second or t.name.eql? "Senegal"
               t.update(eliminated:true)
             end
@@ -62,5 +46,67 @@ class WelcomeController < ApplicationController
         change: @change,
       }
     end
+  end
+
+  def knockout
+    reset_extra
+  end
+
+  def reset_extra
+    User.all.each do |u|
+      session[u.name] = 0
+    end
+  end
+
+  def knockout_team
+    # @team = params["team"].capitalize
+    # require 'rest_client'
+    # @matches = RestClient.get 'https://worldcup.sfg.io/matches', {:accept => :json}
+    # @matches = JSON.parse(@matches)
+    # @stage = @matches.select {|key| key["stage_name"] == params["round"]}
+    # if @stage.find {|key| key["home_team_country"] == nil }
+    #   knockout_future
+    # else
+    #   @match = @stage.find {|key| key["home_team_country"].include? @team}
+    #   if @match == nil
+    #     @match = @stage.find {|key| key["away_team_country"].include? @team}
+    #     @winner = @match["away_team_country"]
+    #     @loser = @match["home_team_country"]
+    #   else
+    #     @loser = @match["away_team_country"]
+    #     @winner = @match["home_team_country"]
+    #   end
+    #   @array = []
+    #   User.all.each do |u|
+    #     u.teams.all.each do |t|
+    #       if t.name.include? @winner
+    #         session[u.name] += 2
+    #       end
+    #       if t.name.include? @loser
+    #         session[u.name] -= 1
+    #       end
+    #     end
+    #     @array.push(u.total+session[u.name])
+    #   end
+    #   @winner = @winner[0..2].upcase
+    #   @loser = @loser[0..2].upcase
+    #   if params["round"].include? "16"
+    #     @round = "16"
+    #   else
+    #     @round = "8"
+    #   end
+    # end
+    # if request.xhr?
+    #   render :json => {
+    #     points: @array,
+    #     winner: @winner,
+    #     loser: @loser,
+    #     round: @round
+    #   }
+    # end
+  end
+
+  def knockout_future
+    binding.pry
   end
 end
