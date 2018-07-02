383e4599256c0b2786d30b2723de0ff2d02c90fd
Pierce <pierce.dejong45@gmail.com>
2018-07-02 13:18:59 -0600

    Fixed Knockout losses and wins

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -17,20 +17,32 @@ class WelcomeController < ApplicationController
       @match_table = RestClient.get 'https://worldcup.sfg.io/matches', {:accept => :json}
       @match_table = JSON.parse(@match_table)
       @knockout_table = @match_table.select {|key| key["stage_name"] != "First stage"}
+      @match_table = @match_table.select {|key| key["stage_name"] == "First stage"}
       User.all.each do |u|
         u.teams.all.each do |t|
-          @group = @group_table.find{|key| key["letter"] == t.group}["ordered_teams"]
-          @team = @group.find{|key| key["country"] == t.name}
-          @knockout = @knockout_table.select {|key| key["winner"]== t.name}
-          if @team != nil
-            points = @team["wins"] + @knockout.length - @team["losses"]
-            if t.value != points
-              t.update(value:points)
+          # @group = @group_table.find{|key| key["letter"] == t.group}["ordered_teams"]
+          # @team = @group.find{|key| key["country"] == t.name}
+          @match_home = @match_table.select {|key| key["home_team_country"] == t.name}
+          @match_away = @match_table.select {|key| key["away_team_country"] == t.name}
+          @knockout_home = @knockout_table.select {|key| key["home_team_country"] == t.name}
+          @knockout_away = @knockout_table.select {|key| key["away_team_country"] == t.name}
+          if @match_home != [] and @match_away != []
+            points = @match_home.select {|key| key["winner"] == t.name}.length + @match_away.select {|key| key["winner"] == t.name}.length
+            draws = @match_home.select {|key| key["winner"] == "Draw"}.length + @match_away.select {|key| key["winner"] == "Draw"}.length
+            loss = 3 - points - draws
+            @score = points - loss
+
+            points = @knockout_home.select {|key| key["winner"] == t.name}.length + @knockout_away.select {|key| key["winner"] == t.name}.length
+            loss = @knockout_home.select {|key| key["winner"] != t.name and key["winner"] != nil}.length + @knockout_away.select {|key| key["winner"] != t.name and key["winner"] != nil}.length
+
+            @score = @score + points*2 - loss
+            if t.value != @score
+              t.update(value:@score)
               @change = true
             end
-            if @team != @group.first and @team != @group.second or t.name.eql? "Senegal"
-              t.update(eliminated:true)
-            end
+            #if @team != @group.first and @team != @group.second or t.name.eql? "Senegal"
+            #  t.update(eliminated:true)
+            #end
           end
         end
       end
